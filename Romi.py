0b405214f9575b6b2f9fae6a8df307c52097538d
Alexander Brown <brownaa@lafayette.edu>
2022-08-04 18:03:40 -0400

    library close

--- Romi.py
+++ Romi.py
@@ -6,12 +6,8 @@ import time
 
 class Romi:
     def __init__(self,sim=False,port='COM5',baud=115200):
-        self.ser = serial.Serial(port,baudrate=baud,timeout=.1)
-        print("initializing")
-        time.sleep(.5)
-        #self.ser.open()
-        time.sleep(2)
-        print("done")
+        self.port = port
+        self.baud= baud
         self.leftCommand = 0
         self.rightCommand = 0
         self.s1Command = 90
@@ -26,15 +22,78 @@ class Romi:
         self.Sim = sim
         self.goodReply = False
         self.datastring = ''
+        if(self.Sim):
+            self.initSim()
+        else:
+            self.initSerial()
+
+    def initSim(self):
+        from controller import Robot, Motor
+        self.simromi = Robot()
+        self.simLeftMotor = self.simromi.getDevice('motor_left')
+        self.simRightMotor = self.simromi.getDevice('motor_right')
+        self.simS1 = self.simromi.getDevice("s1")
+        self.simS3 = self.simromi.getDevice("s3")
+        self.simS2 = self.simromi.getDevice("s2")
+
+        self.simLeftMotor.setPosition(float('inf'))
+        self.simRightMotor.setPosition(float('inf'))
+
+
+    def initSerial(self):
+        import serial
+        self.ser = serial.Serial(self.port,baudrate=self.baud,timeout=.1)
+        print("initializing")
+        time.sleep(.5)
+        #self.ser.open()
+        time.sleep(2)
+        print("done")
+
+    def clamp(self,val,min,max):
+        assert min<max
+        if(val<min):
+            val=min
+        elif val>max:
+            val=max
+        return val
 
     def update(self,leftCommand=0,rightCommand=0,s1Command=90,s2Command=90,s3Command=90):
         #save commands to object
-        self.leftCommand = leftCommand
-        self.rightCommand = rightCommand
-        self.s1Command = s1Command
-        self.s2Command = s2Command
-        self.s3Command = s3Command
+        self.leftCommand = self.clamp(leftCommand,-400,400)
+        self.rightCommand = self.clamp(rightCommand,-400,400)
+        self.s1Command = self.clamp(s1Command,0,180)
+        self.s2Command = self.clamp(s2Command,0,180)
+        self.s3Command = self.clamp(s3Command,0,180)
 
+        if(self.Sim):
+            self.updateSim()
+        else:
+            self.updateHW()
+
+
+
+    def updateSim(self):
+        #convert motor speed commands to rad/s
+        wb_leftWheel = self.leftCommand*16.0/400
+        wb_rightWheel = self.rightCommand*16.0/400
+        #convert servo commands to webots coordinates
+        #servo commands at nominal: 90,0,68
+        #servo feedback at nominal: 152, 303,111
+        #simulation nominal:  .53rad,0m, 1.2rad
+        wb_s1c = .53+(self.s1Command-90)*3.1415/180
+        wb_s2c = .017/180*self.s2Command
+        wb_s3c = 1.2-(self.s3Command-68)*3.1415/180
+        #now send to simulation
+        self.simLeftMotor.setVelocity(wb_leftWheel)
+        self.simRightMotor.setVelocity(wb_rightWheel)
+        self.simS1.setPosition(wb_s1c)
+        self.simS2.setPosition(wb_s2c)
+        self.simS3.setPosition(wb_s3c)
+        #now get feedback from servos
+
+        self.encRight = self.romi.simromi.getMotorPosition()*
+
+    def updateHW(self):
         sendstr = "!"+format(self.leftCommand,'d')+","+format(self.rightCommand,'d')+","+format(self.s1Command,'d')+","+format(self.s2Command,'d')+","+format(self.s3Command,'d')+"\r\n"
         # print("sending:")
         # print(sendstr.encode())
@@ -58,5 +117,5 @@ class Romi:
             except:
                 print("serial parsing failed")
         else:
-            print("BAD LINE")
+            #print("BAD LINE")
             self.goodReply = False
---
+++ romi_python_staticposition.py
@@ -0,0 +1,56 @@
+from Romi import Romi
+import matplotlib.pyplot as plt
+import time
+import keyboard
+
+
+
+
+#start a romi object on com 5
+romi = Romi(port='COM5')
+
+#open a file
+f = open("data.txt","w")
+
+starttime = time.time()
+currtime = time.time()-starttime
+motors = 0
+s1c=90
+s2c=90
+s3c=90
+key = ''
+
+def procKeys(event):
+    global s1c,s2c,s3c,key
+    key = event.name
+    if(key=="u"):
+        s1c+=1
+    elif(key=="j"):
+        s1c-=1
+    elif(key=="i"):
+        s2c+=1
+    elif(key=="k"):
+        s2c-=1
+    elif(key=="o"):
+        s3c+=1
+    elif(key=="l"):
+        s3c-=1
+
+#set up keyboard callback
+keyboard.hook(procKeys,suppress=False)
+
+while key!='q':
+    key = keyboard.read_key()
+    print("key: "+key)
+    currtime = time.time()-starttime
+    #update takes leftMotor,rightMotor,servo1,servo2,servo3
+    print("s1c: "+str(s1c)+" s2c: "+str(s2c)+"s3c: "+str(s3c))
+    romi.update(motors,motors,s1c,s2c,s3c)
+    if(romi.goodReply):
+        print("s1f: "+str(romi.s1Pos)+" s2f: "+str(romi.s2Pos)+" s3f: "+str(romi.s3Pos))
+    #print(str(currtime)+","+romi.datastring)
+    time.sleep(0.1)
+
+
+
+romi.ser.close()
